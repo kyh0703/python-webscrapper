e508a5acb7cb2faa01022f42e11a14c59f2ed19a
Kimyeonho <kyh0703@nate.com>
2022-07-11 14:27:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-from flask import Flask
-
-app = Flask("SuperScrapper")
-app.run()
-=======
 import math
 import requests
 from bs4 import BeautifulSoup
@@ -13,7 +7,7 @@ indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
 
 limit = 50
 
-search_count_pages_text = indeed_soup.find("div", {"id":"searchCountPages"}).text
+search_count_pages_text = indeed_soup.find("div", {"id": "searchCountPages"}).text
 # 예시결과: 1페이지 결과 1,466건
 
 count_text = search_count_pages_text.split()
@@ -21,17 +15,16 @@ count_text = search_count_pages_text.split()
 contents_max_count = count_text[-1:][0]
 # 1,467건
 
-contents_max_count = contents_max_count.replace(',','')
+contents_max_count = contents_max_count.replace(",", "")
 # 1467건
 
-contents_max_count = contents_max_count.replace('건','')
+contents_max_count = contents_max_count.replace("건", "")
 # 1467 ->type is str
 
 contents_max_count = int(contents_max_count)
 # 1467 ->type is int
 
-max_page_num = math.ceil(contents_max_count/limit)
+max_page_num = math.ceil(contents_max_count / limit)
 # 50으로 나누고 올림
 
 print(max_page_num)
->>>>>>> b5fc2b12c33e23c6af6daab6c18234f9764a51e5
