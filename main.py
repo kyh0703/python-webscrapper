7bb8590106e119a45f4e7172d00f352bad8d9312
Kimyeonho <kyh0703@nate.com>
2022-07-13 10:03:05 +0900

    [Update]

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
-import math
-import requests
-from bs4 import BeautifulSoup
+from flask import Flask, render_template, request, redirect
 
-indeed_result = requests.get("https://kr.indeed.com/jobs?q=python&limit=50")
-indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
+app = Flask("SuperScrapper")
 
-limit = 50
 
-search_count_pages_text = indeed_soup.find("div", {"id": "searchCountPages"}).text
-# 예시결과: 1페이지 결과 1,466건
+@app.route("/")
+def home():
+    return render_template("potato.html")
 
-count_text = search_count_pages_text.split()
-# ['1페이지', '결과', '1,466건']
-contents_max_count = count_text[-1:][0]
-# 1,467건
 
-contents_max_count = contents_max_count.replace(",", "")
-# 1467건
+@app.route("/<username>")
+def potato(username):
+    return f"Hello {username} how are you doing?"
 
-contents_max_count = contents_max_count.replace("건", "")
-# 1467 ->type is str
 
-contents_max_count = int(contents_max_count)
-# 1467 ->type is int
+@app.route("/report")
+def report():
+    word = request.args.get("word")
+    if word:
+        word = word.lower()
+    else:
+        return redirect("/")
+    return render_template("report.html", searchingBy=word)
 
-max_page_num = math.ceil(contents_max_count / limit)
-# 50으로 나누고 올림
 
-print(max_page_num)
+app.run()
